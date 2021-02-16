63d2c25166939133a69245ae3041c03cb1f00498
ana_guerrero <tcolaga@gmail.com>
2021-02-16 18:29:05 +0100

    add cypress e2e test

--- cypress/integration/ login.spec.ts
+++ cypress/integration/ login.spec.ts
@@ -1,92 +1,56 @@
 
-describe('login scene specs', () => {
-  beforeEach(() => {
+describe('Login specs', () => {
+  it('visit the login page', () => {
     cy.visit('/');
   });
 
-  it('"user" input should get the focus when clicking on it', () => {
+  it('should name input has the focus when it clicks on it', () => {
     // Arrange
 
     // Act
-    cy.findByRole('textbox').as('userInput');
+    cy.visit('/');
+
+    // Assert
+    cy.findByLabelText('Usuario *').as('userInput');
     cy.get('@userInput').click();
-
-    // Asert
-    cy.get('@userInput').should('have.focus');
   });
 
-  it('"password" input should get the focus when clicking on it', () => {
+  it('should show an error when type invalid credentials', () => {
     // Arrange
+    const user: string = 'admin';
+    const password: string = '1234';
+    // cy.on(useSnackbarContext(), cy.stub().as('showMessageStub'));
 
     // Act
+    cy.visit('/');
+    cy.findByLabelText('Usuario *').as('userInput');
     cy.findByLabelText('Contraseña *').as('passwordInput');
-    cy.get('@passwordInput').click();
+    cy.findByRole('button', { name: 'Login' }).as('loginButton');
 
-    // Asert
-    cy.get('@passwordInput').should('have.focus');
-  });
+    // Assert
 
-  it('if "user" input is empty, it should show error message when blur after clicking on it', () => {
-    // Arrange
-    const errorMsg = 'Debe informar el campo';
-
-    // Act
-    cy.findByRole('textbox').as('userInput');
-    cy.get('@userInput').click();
-    cy.get('@userInput').blur();
-
-    // Asert
-    cy.get(
-      'p[class="MuiFormHelperText-root MuiFormHelperText-contained Mui-error"]'
-    ).should('have.text', errorMsg);
-  });
-
-  it('if "password" input is empty, it should show error message when blur after clicking on it', () => {
-    // Arrange
-    const errorMsg = 'Debe informar el campo';
-
-    // Act
-    cy.findByLabelText('Contraseña *').as('passwordInput');
-    cy.get('@passwordInput').click();
-    cy.get('@passwordInput').blur();
-
-    // Asert
-    cy.get(
-      'p[class="MuiFormHelperText-root MuiFormHelperText-contained Mui-error"]'
-    ).should('have.text', errorMsg);
-  });
-
-  it('should show alert when clicking on "login" button with wrong values in the inputs', () => {
-    // Arrange
-    const userWrongValue = 'John';
-    const passwordWrongValue = '12345';
-
-    // Act
-    cy.findByRole('textbox').as('userInput');
-    cy.findByLabelText('Contraseña *').as('passwordInput');
-    cy.findByRole('button').as('loginBtn');
-    cy.get('@userInput').type(userWrongValue);
-    cy.get('@passwordInput').type(passwordWrongValue);
-    cy.get('@loginBtn').click();
-
-    // Asert
-    cy.findByRole('alert').should('be.visible');
-  });
-
-  it('should navigate to "submodule-list" after clicking "login" button with correct values in both inputs', () => {
-    // Arrange
-    const user = 'admin';
-    const password = 'test';
-
-    // Act
-    cy.findByRole('textbox').as('userInput');
-    cy.findByLabelText('Contraseña *').as('passwordInput');
-    cy.findByRole('button').as('loginBtn');
     cy.get('@userInput').type(user);
     cy.get('@passwordInput').type(password);
-    cy.get('@loginBtn').click();
+    cy.get('@loginButton').click();
+    cy.findByText('Usuario y/o password no válidos');
+  });
 
-    // Asert
-    cy.url().should('equal', 'http://localhost:8080/#/submodule-list');
+  it('should navigate to submodule-list url when type valid credentials', () => {
+    // Arrange
+    const user: string = 'admin';
+    const password: string = 'test';
+
+    // Act
+    cy.visit('/');
+    cy.findByLabelText('Usuario *').as('userInput');
+    cy.findByLabelText('Contraseña *').as('passwordInput');
+    cy.findByRole('button', { name: 'Login' }).as('loginButton');
+
+    // Assert
+
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+    cy.get('@loginButton').click();
+    cy.url().should('eq', 'http://localhost:8080/#/submodule-list');
   });
 });
